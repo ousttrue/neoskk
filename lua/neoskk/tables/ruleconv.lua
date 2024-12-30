b0cff55e32470d6e0e1198a37d644236fd483793
ousttrue <ousttrue@gmail.com>
2024-12-31 03:31:12 +0900

    to_kana

--- lua/neoskk/tables/ruleconv.lua
+++ lua/neoskk/tables/ruleconv.lua
@@ -8,41 +8,9 @@ local match_rules = require "neoskk.tables.match_rules"
 ---@param _feed string?
 ---@return string 確定変換済み
 ---@return string 未使用のキー入力
-local function ruleconv(rules, src, _feed)
-  -- 出力文字列
-  local output = ""
-  -- 未使用のキー入力
-  local feed = _feed and _feed or ""
-  -- 部分一致が複数在って後続を見るまで確定できない
-  ---@type MatchedKanaRule?
-  local candidate = nil
-
-  for key in src:gmatch "." do
-    -- 一文字ずつ処理する
-    feed = feed .. key
-    local match = match_rules(candidate and candidate.prefix_matches or rules, feed)
-    local tmp_output
-    tmp_output, feed, candidate = match:resolve(candidate)
-    if tmp_output then
-      output = output .. tmp_output
-    end
-  end
-
-  while #feed > 0 do
-    -- 入力の残り
-    local match = match_rules(candidate and candidate.prefix_matches or rules, feed)
-    local tmp_output, tmp_feed, tmp_candidate = match:resolve(candidate)
-    candidate = tmp_candidate
-    if tmp_output then
-      output = output .. tmp_output
-    end
-    if feed == tmp_feed then
-      break
-    end
-    feed = tmp_feed
-  end
-
-  return output, feed
+local function ruleconv(rules, feed, candidate)
+  local match = match_rules(rules, feed)
+  return match:resolve(candidate)
 end
 
 return ruleconv
--- tests/kana_spec.lua
+++ tests/kana_spec.lua
@@ -2,10 +2,43 @@ local ruleconv = require "neoskk.tables.ruleconv"
 local KanaRules = require "neoskk.tables.KanaRules"
 
 ---@param src string
----@return string out
+---@return string src
 ---@return string feed
 local function to_kana(src)
-  return ruleconv(KanaRules, src)
+  local feed = ""
+  -- 出力文字列
+  local output = ""
+  -- 部分一致が複数在って後続を見るまで確定できない
+  ---@type MatchedKanaRule?
+  local candidate = nil
+
+  for key in src:gmatch "." do
+    -- 一文字ずつ処理する
+    feed = feed .. key
+    -- local match = match_rules(candidate and candidate.prefix_matches or rules, feed)
+    local tmp_output
+    tmp_output, feed, candidate = ruleconv(candidate and candidate.prefix_matches or KanaRules, feed, candidate)
+    -- tmp_output, feed, candidate = match:resolve(candidate)
+    if tmp_output then
+      output = output .. tmp_output
+    end
+  end
+
+  while #feed > 0 do
+    -- 入力の残り
+    -- local match = match_rules(candidate and candidate.prefix_matches or rules, feed)
+    local tmp_output, tmp_feed, tmp_candidate = ruleconv(candidate and candidate.prefix_matches or KanaRules, feed, candidate)
+    candidate = tmp_candidate
+    if tmp_output then
+      output = output .. tmp_output
+    end
+    if feed == tmp_feed then
+      break
+    end
+    feed = tmp_feed
+  end
+
+  return output, feed
 end
 
 -- TODO 辞書を引数にする
